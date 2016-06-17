246dea744d5c2abb635830481b774737419cb707
Don Park <donp@donp.org>
2016-06-17 10:26:01 -0700

    load the config and start the poll

---
+++ index.js
@@ -0,0 +1,14 @@
+// node
+var fs = require('fs')
+
+// npm
+var Hjson = require('hjson');
+
+// local
+var poll = require('./lib/superpoll.js')
+
+// config
+var hjson = fs.readFileSync('./config.hjson', {encoding: 'utf8'})
+var config = Hjson.parse(hjson)
+
+poll.setup(config)
