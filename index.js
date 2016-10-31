a3e908aae80a009e65014a49577c3e3ffba68fdf
Don Park <donp@donp.org>
2016-10-30 17:05:06 -0700

    calculate diffs

--- index.js
+++ index.js
@@ -58,11 +58,49 @@ function gopoll(){
             function(orderbook){
               console.log(orderbook.exchange,
                           orderbook.market,
-                          'orderbook head',
+                          'top ask',
                           orderbook.asks[0],
+                          'top bid',
                           orderbook.bids[0])
               poll.insert(orderbook)
+              return orderbook
             })
       })
     }, err => console.log('Orderbook phase err', err.message))
+    .then( orderbooks => {
+      orderbooks.forEach( exchange => {
+        exchange.forEach( market => {
+          console.log('** nexto', market.date, market.exchange, market.market)
+          let ago = config.system.poll_seconds * 1000
+          let now = new Date()
+          poll
+            .last(market.market.base, market.market.quote, now, ago)
+            .then(function(cursor){
+              cursor
+              .toArray()
+              .then(function(books) {
+                // inefficient but...
+                let partialBooks = books.filter(function(book){ return book.exchange == market.exchange })
+                if(partialBooks.length >= 2) {
+                  partialBooks
+                    .forEach(function(book){
+                      let delay = (market.date-book.date)/1000
+                      console.log('fetch ', book.exchange, book.market.base, book.market.quote,
+                                  delay.toFixed(2)+'s')
+                    })
+                  let askChg = parseFloat(partialBooks[0].asks[0][0]) -
+                               parseFloat(partialBooks[1].asks[0][0])
+                  let bidChg = parseFloat(partialBooks[0].bids[0][0]) -
+                               parseFloat(partialBooks[1].bids[0][0])
+                  console.log('diff', market.exchange, market.market.base, market.market.quote,
+                              'askChg', askChg, 'bidChg', bidChg)
+                } else {
+                  console.log('no previous timeslot for', market.exchange, market.market.base, market.market.quote)
+                }
+              })
+            })
+
+        })
+      })
+    })
 }
